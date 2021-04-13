f88a57e9f6e557bd70ebffce73b6a7b612eb09db
RubikonZ <rubenzurr@gmail.com>
2021-04-13 14:33:31 +0300

    Added everything that was needed and expected. Subject for future possible refactoring

--- project-etch-A-sketch/etch-a-sketch-js.js
+++ project-etch-A-sketch/etch-a-sketch-js.js
@@ -1,11 +1,11 @@
 
 const container = document.querySelector('.container')
 const btn = document.querySelector('.btn')
-btn.addEventListener('click', () => createNewGrid())
+btn.addEventListener('click', () => createNewGrid(prompt('Input size of the square grid (Should be <= 100)')))
 
-function createNewGrid() {
+function createNewGrid(grid_size=16) {
     clearGrid(document.querySelector('.container'));
-    let grid_size = prompt('Input size of the square grid (Should be <= 100)')
+    window.scrollTo(0, 25); // On button press takes us to the full grid
     for (i = 1; i <= grid_size**2; i++) {
         const new_div = document.createElement('div');
         new_div.classList.add('child');
@@ -14,10 +14,11 @@ function createNewGrid() {
         //new_div.textContent = `#${i}`;
         container.appendChild(new_div)
     }
-
+    
+    let back_color;
+    container.addEventListener('mouseenter', () => back_color = `#${(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6)}`);
     let pixels = document.querySelectorAll('.child')
-
-    pixels.forEach((pixel) => pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = 'black') )
+    pixels.forEach((pixel) => pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = back_color) )
 }
 
 function clearGrid(parent) {
@@ -26,15 +27,7 @@ function clearGrid(parent) {
     }
 }
 
-for (i = 1; i <= 256; i++) {
-    const new_div = document.createElement('div');
-    //new_div.textContent = `#${i}`;
-    new_div.style.width = `${(window.innerWidth - 20)/16}px`;
-    new_div.style.height = `${window.innerHeight/16}px`;
-    new_div.style.fontSize = '10px';
-    new_div.classList.add('child');
-    container.appendChild(new_div);
-}
-let pixels = document.querySelectorAll('.child')
+createNewGrid()
 
-pixels.forEach((pixel) => pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = 'black') )
+
+
